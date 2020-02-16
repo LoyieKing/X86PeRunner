85228780f9e7fa97076f8e52a3adfced6a01baaf
Loyie King <loyie924558375@hotmail.com>
2020-02-16 13:30:27 +0800

    add capstone/unicorn with arm32 support

--- X86PeRunner/main.cpp
+++ X86PeRunner/main.cpp
@@ -1,12 +1,20 @@
 #include<stdio.h>
-#include "capstone/include/capstone/capstone.h"
+#include<platform.h>
+#include<capstone\capstone.h>
+#include<unicorn\unicorn.h>
 
 int main()
 {
-	int major = 0;
-	int minor = 0;
-	cs_version(&major, &minor);
-	printf("Hello World!");
+	int cs_major = 0;
+	int cs_minor = 0;
+	cs_version(&cs_major, &cs_minor);
+	printf("Capstone version:%d.%d\n", cs_major, cs_minor);
+
+	unsigned int uc_major = 0;
+	unsigned int uc_minor = 0;
+
+	uc_version(&uc_major, &uc_minor);
+	printf("Unicorn version:%d.%d\n", uc_major, uc_minor);
 
 	return 0;
 }
